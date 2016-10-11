22b3a99357fea960add928df7a960712563ccc4c
astrong19 <astrong@wri.org>
2016-10-11 13:35:25 -0400

    postprocess script added for glad

--- postprocess/glad_maps.py
+++ postprocess/glad_maps.py
@@ -7,135 +7,119 @@ import os
 import time
 import datetime
 from os import walk
+import sys
+import logging
 
 #Check out ArcGIS Extensions
 arcpy.CheckOutExtension("Spatial")
 arcpy.env.overwriteOutput = True
-arcpy.env.scratchWorkspace = r"D:\temp\environment"
 
-rasters = [
-r"http://umd-landsat-alerts.s3.amazonaws.com/borneo_day2016.tif",
-r"http://umd-landsat-alerts.s3.amazonaws.com/peru_day2016.tif",
-# r"http://umd-landsat-alerts.s3.amazonaws.com/roc_day2016.tif",
-# r"http://umd-landsat-alerts.s3.amazonaws.com/brazil_day2016.tif"
-]
-
-new_rasters = [
-r"D:\GIS Data\GFW\glad\raster\borneo_day2016.tif",
-r"D:\GIS Data\GFW\glad\raster\peru_day2016.tif",
-# r"D:\GIS Data\GFW\glad\raster\roc_day2016.tif",
-# r"D:\GIS Data\GFW\glad\raster\brazil_day2016.tif"
-]
-
-#paths to past points for analysis
-borneo_past = r"D:\GIS Data\GFW\glad\past_points\borneo_past.shp"
-peru_past = r"D:\GIS Data\GFW\glad\past_points\peru_past.shp"
-roc_past = r"D:\GIS Data\GFW\glad\past_points\roc_past.shp"
-brazil_past = r"D:\GIS Data\GFW\glad\past_points\brazil_past.shp"
-
-#Paths to latest points
-borneo_latest = r"D:\GIS Data\GFW\glad\latest_points\borneo_latest.shp"
-peru_latest = r"D:\GIS Data\GFW\glad\latest_points\peru_latest.shp"
-roc_latest = r"D:\GIS Data\GFW\glad\latest_points\roc_latest.shp"
-brazil_latest = r"D:\GIS Data\GFW\glad\latest_points\brazil_latest.shp"
-
-#Grab max values from past rasters
-borneo_max = arcpy.SearchCursor(borneo_past, "", "", "","GRID_CODE D").next().getValue("GRID_CODE")
-peru_max = arcpy.SearchCursor(peru_past, "", "", "","GRID_CODE D").next().getValue("GRID_CODE")
-roc_max = arcpy.SearchCursor(roc_past, "", "", "","GRID_CODE D").next().getValue("GRID_CODE")
-brazil_max = arcpy.SearchCursor(brazil_past, "", "", "","GRID_CODE D").next().getValue("GRID_CODE")
-
-#set global variables for maps
 borneo_mxd = arcpy.mapping.MapDocument(r"D:\GIS Data\GFW\glad\maps\mxds\borneo.mxd")
 peru_mxd = arcpy.mapping.MapDocument(r"D:\GIS Data\GFW\glad\maps\mxds\peru.mxd")
 roc_mxd = arcpy.mapping.MapDocument(r"D:\GIS Data\GFW\glad\maps\mxds\roc.mxd")
 brazil_mxd = arcpy.mapping.MapDocument(r"D:\GIS Data\GFW\glad\maps\mxds\brazil.mxd")
-density = r"D:\GIS Data\GFW\glad\maps\density_rasters\borneo_density.tif"
-
-def convert_rasters():
-
-    for raster in rasters:
-        file_name = raster.split("/")[-1]
-        print "downloading %s and calculating stats" %(file_name)
-        path_var = os.path.join (r"D:\GIS Data\GFW\glad\raster", file_name)
-        urllib.urlretrieve(url,path_var)
-        arcpy.CalculateStatistics_management(raster)
 
 
-    for new_raster in new_rasters:
-        file_name = new_raster.split("/")[-1]
-        prefix = file_name.split("_")[0] + "_max"
-        latest = file_name.split("_")[0] + "_latest.tif"
-        print "extracting values for %s" %(file_name)
-        where_clause = "Value > " + str(prefix)
-        raster_extract = ExtractByAttributes(new_raster, where_clause)
-        raster_extract.save(os.path.join (r"D:\GIS Data\GFW\glad\latest_points", latest))
+def post_process(layerdef):
+    """
+    Update the date value in the layerspec table-- required for the time slider to work properly
+    :param layerdef: the layerdef
+    :return:
+    """
+    logging.debug('starting postprocess glad maps')
+    print layerdef.source
 
-    files = []
-    my_path = r"D:\GIS Data\GFW\glad\latest_points"
-    for (dirpath, dirnames, filenames) in walk(my_path):
-        files.extend(filenames)
-        break
+    olddata_hash = {}
+    #replace path with D:\GIS Data\GFW\glad\past_points
+    past_points = [
+    r"D:\GIS Data\GFW\glad\past_points\borneo_day2016.shp",
+    r"D:\GIS Data\GFW\glad\past_points\peru_day2016.shp",
+    r"D:\GIS Data\GFW\glad\past_points\roc_day2016.shp",
+    r"D:\GIS Data\GFW\glad\past_points\brazil_day2016.shp"
+    ]
 
-    file_paths = []
-    for file in files:
-        f = os.path.join(my_path, file)
-        file_paths.append(f)
+    latest_rasters = [
+    r"D:\GIS Data\GFW\glad\latest_points\borneo_day2016.tif",
+    r"D:\GIS Data\GFW\glad\latest_points\peru_day2016.tif",
+    r"D:\GIS Data\GFW\glad\latest_points\brazil_day2016.tif",
+    r"D:\GIS Data\GFW\glad\latest_points\roc_day2016.tif"
+    ]
+    new_points = []
 
-    for path in file_paths:
-        n = path.split("\\")[-1]
-        name = n.split("_")[0]
-        output = os.path.join(my_path, name + ".shp")
-        if "latest" in path:
-            arcpy.RasterToPoint_conversion(file, output, "Value")
+    for point in past_points:
+        point_name = os.path.basename(point)
+        olddata_hash[point_name] = arcpy.SearchCursor(point, "", "", "","GRID_CODE D").next().getValue("GRID_CODE")
 
+    for ras in layerdef.source:
+        if "day" in ras:
+            ras_name = os.path.basename(ras)
+            shp_name = ras_name.replace(".tif", ".shp")
+            where_clause = "Value > " + str(olddata_hash[shp_name])
+            raster_extract = ExtractByAttributes(ras, where_clause)
+            latest_raster = raster_extract.save(os.path.join (r"D:\GIS Data\GFW\glad\latest_points", ras_name))
+            # latest_rasters.append(latest_raster)
+            print "new values for %s extracted" %(ras_name)
+        else:
+            pass
 
-#Archive
-def archive(point):
-    print "archiving %s" %(point)
-    ts = time.time()
-    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d')
-    file_name = point.split("//")[-1].replace(".shp","")
-    path_var = os.path.join (r"D:\GIS Data\GFW\glad\archive", file_name + "_" + timestamp + ".shp")
-    arcpy.Copy_management(point, path_var)
+    #failed here
+    for ras in latest_rasters:
+        ras_name = os.path.basename(ras).replace(".tif", ".shp")
+        output = os.path.join(os.path.dirname(ras), ras_name)
+        new_point = arcpy.RasterToPoint_conversion(ras, output, "Value")
+        new_points.append(output)
+        print "converted %s to points" %(ras)
 
+    for newp in new_points:
+        for pastp in past_points:
+            if os.path.basename(newp) == os.path.basename(pastp):
+                arcpy.Copy_management(newp, pastp)
+                print "copied %s to %s" %(newp, pastp)
 
-#Replace old points
-def replace(past_point, latest_point):
-    print "replacing past points"
-    arcpy.Copy_management(latest_point, past_point)
+    for newp in new_points:
+        outKDens = KernelDensity(newp, "NONE", "", "", "HECTARES")
+        path = r"D:\GIS Data\GFW\glad\maps\density_rasters"
+        name = os.path.basename(newp).replace(".shp", "")
+        output = os.path.join(path, name + "_density.tif")
+        outKDens.save(output)
 
-#Run kernel density analysis
-def kernel(latest_point):
-    print "running density analysis for %s" %(latest_point)
-    ts = time.time()
-    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y%m%d')
-    outKDens = KernelDensity(latest_point, "NONE", "", "", "HECTARES")
-    path = r"D:\GIS Data\GFW\glad\maps\density_rasters"
-    n = latest_point.split("\\")[-1]
-    name = n.split("_")[0]
-    output = os.path.join(path, name + "_density.tif")
-    outKDens.save(output)
+    for layer in layerdef.source:
+        if "peru" in layerdef.source:
+            make_maps(peru_mxd)
+        if "roc" in layerdef.source:
+            make_maps(roc_mxd)
+        if "brazil" in layerdef.source:
+            make_maps(brazil_mxd)
+        if "borneo" in layerdef.source:
+            make_maps(borneo_mxd)
+        else:
+            pass
 
-#find extent of GADM boundary, clip data, and add to mxd file
-def select(mxd):
+def make_maps(mxd):
 
-    #set variables
+    logging.debug('starting postprocess make maps')
+
     gadm = r"D:\GIS Data\Country Boundaries\country boundaries.gdb\countries"
+    global ISO
+    global name
+    global density
 
     if mxd == borneo_mxd:
         ISO = "'BRN'"
-        name = "borneo"
+        name = "IDN13"
+        density = r"D:\GIS Data\GFW\glad\maps\density_rasters\borneo_day2016_density.tif"
     if mxd == peru_mxd:
-        ISO = "PER"
-        name = "peru"
+        ISO = "'PER'"
+        name = "PER"
+        density = r"D:\GIS Data\GFW\glad\maps\density_rasters\peru_day2016_density.tif"
     if mxd == brazil_mxd:
-        ISO = "BRA"
-        name = "brazil"
+        ISO = "'BRA'"
+        name = "BRA"
+        density = r"D:\GIS Data\GFW\glad\maps\density_rasters\brazil_day2016_density.tif"
     if mxd == roc_mxd:
-        ISO = "COG"
-        name = "roc"
-
+        ISO = "'COG'"
+        name = "COG"
+        density = r"D:\GIS Data\GFW\glad\maps\density_rasters\roc_day2016_density.tif"
 
     arcpy.MakeFeatureLayer_management(gadm,"lyr")
     SQL_query = " code_iso3 = "+ISO
@@ -170,23 +154,7 @@ def select(mxd):
     arcpy.RefreshActiveView()
     arcpy.mapping.RemoveLayer(df,gadm_lyr)
 
-#apply symbology and export
-def export():
-
-    #set variables
-    if mxd == borneo_mxd:
-        ISO = "'BRN'"
-        name = "IDN13"
-    if mxd == peru_mxd:
-        ISO = "PER"
-        name = "PER"
-    if mxd == brazil_mxd:
-        ISO = "BRA"
-        name = "BRA"
-    if mxd == roc_mxd:
-        ISO = "COG"
-        name = "COG"
-
+    #export
     print "adding symbology and exporting"
     ts = time.time()
     time_year = datetime.datetime.fromtimestamp(ts).strftime("%Y")
@@ -198,22 +166,3 @@ def export():
     output = os.path.join(r"F:\climate\glad_maps", name + "_" + time_year + "_" + time_week + ".png")
     arcpy.mapping.ExportToPNG(mxd, output)
     print "map created"
-
-convert_rasters()
-archive_point(borneo_past)
-# archive_point(roc_past)
-archive_point(peru_past)
-# archive_point(brazil_past)
-replace(borneo_past, borneo_latest)
-# replace(roc_past, roc_latest)
-replace(peru_past, peru_latest)
-# replace(brazil_past, brazil_latest)
-kernel(borneo_latest)
-# kernel(roc_latest)
-kernel(peru_latest)
-# kernel(brazil_latest)
-select(borneo_mxd)
-# select(roc_mxd)
-select(peru_mxd)
-# select(brazil_mxd)
-export()
